8d54ade3f3e716bb72d3727c9629b2be8ccc4b8f
offshoregithub <65126846+offshoregithub@users.noreply.github.com>
2020-05-10 18:56:30 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,4 +3,10 @@ apt-get update
 apt-get install python -y
 apt-get install python3 -y 
 apt-get intall wireshark -y
+apt install python3-pip -y
+apt install python-pip -y
+pip install fire
+pip install scapy
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
 exit
